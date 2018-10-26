173598965eb5ed534ed130e8841003705cd9a7da
Kevin Searcy <kevinleesearcy@gmail.com>
2018-10-26 14:26:44 -0500

    First Open Source Push

---
+++ Install_Helper/Conversion_app 05-22-2017 3pm/Conversion_app/TestApp/TestApp/32marketClass.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace TestApp
+{
+    public class User2
+    {
+        public int idUser { get; set; }
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string phoneNumber { get; set; }
+        public string email { get; set; }
+        public string creationDate { get; set; }
+        public DateTime? lastActivity { get; set; }
+        public int idWarehouse { get; set; }
+        public int permissionLevel { get; set; }
+        public bool? isEmployee { get; set; }
+        public int idParentUser { get; set; }
+        public List<object> subUsers { get; set; }
+    }
+
+    public class User
+    {
+        public User2 user { get; set; }
+        public List<Credentials> credentials { get; set; }
+        public List<object> cards { get; set; }
+        public List<Tags> tags { get; set; }
+        public List<object> userNames { get; set; }
+        public List<object> passwords { get; set; }
+        public List<UserSettings> userSettings { get; set; }
+        public List<object> hashedLogins { get; set; }
+        public List<object> actionPermissions { get; set; }
+    }
+
+    public class Credentials
+    { 
+        public int idUserCredentials { get; set; }
+        public string accountCode { get; set; }
+        public string email { get; set; }
+        public string password { get; set; }
+        public string salt { get; set; }
+        public bool? isEnabled { get; set; }
+        public int? idUser { get; set; }
+        public bool isEmailVerified { get; set; }
+        public string lastPasswordChangeDate { get; set; }
+    }
+
+    public class UserSettings
+    { 
+        public int idUser { get; set; }
+        public int userSettingType { get; set; }
+        public string value { get; set; }
+        public bool? isEnabled { get; set; }
+        public int idUserSetting { get; set; }
+    }
+
+    public class Tags
+    { 
+        public int idUserDogTag { get; set; }
+        public string upc { get; set; }
+        public int idUser { get; set; }
+        public bool isEnabled { get; set; }            
+    }
+
+    public class FingerPrint
+    {
+        private string _data;
+
+        public int idUserFingerprint { get; set; }
+        public string data 
+        {
+            get
+            { 
+                byte[] d = Convert.FromBase64String(_data);
+                return Encoding.Unicode.GetString(d);                
+            }
+            set
+            {
+                _data = value;
+            }
+        }
+        public int idUser { get; set; }
+    }
+
+    public class Value
+    {
+        public string _id { get; set; }
+        public string _rev { get; set; }
+        public double? marketBalance { get; set; }
+        public double? payrollBalance { get; set; }
+        public int contractType { get; set; }
+        public User user { get; set; }
+        public List<FingerPrint> fingerPrints { get; set; }
+        public bool isSynced { get; set; }
+        public DateTime? lastDeposit { get; set; }
+    }
+
+    public class Row
+    {
+        public string id { get; set; }
+        public int key { get; set; }
+        public Value value { get; set; }
+    }
+
+    public class RootObject
+    {
+        public int total_rows { get; set; }
+        public int offset { get; set; }
+        public List<Row> rows { get; set; }
+    }
+}
---
+++ Install_Helper/Conversion_app 05-22-2017 3pm/Conversion_app/TestApp/TestApp/Form1.Designer.cs
@@ -0,0 +1,338 @@
+﻿using System.Configuration;
+namespace TestApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openFileDialog32 = new System.Windows.Forms.OpenFileDialog();
+            this.tabPage4 = new System.Windows.Forms.TabPage();
+            this.txtResultsLog = new System.Windows.Forms.TextBox();
+            this.avantiInsert = new System.Windows.Forms.Button();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.Load32 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.openFileDialog32Couch = new System.Windows.Forms.OpenFileDialog();
+            this.tabPage4.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabControl1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // openFileDialog32
+            // 
+            this.openFileDialog32.FileName = "openFileDialog1";
+            this.openFileDialog32.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
+            // 
+            // tabPage4
+            // 
+            this.tabPage4.Controls.Add(this.txtResultsLog);
+            this.tabPage4.Controls.Add(this.avantiInsert);
+            this.tabPage4.Controls.Add(this.textBox6);
+            this.tabPage4.Location = new System.Drawing.Point(4, 22);
+            this.tabPage4.Name = "tabPage4";
+            this.tabPage4.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage4.Size = new System.Drawing.Size(539, 640);
+            this.tabPage4.TabIndex = 3;
+            this.tabPage4.Text = "Avanti";
+            this.tabPage4.UseVisualStyleBackColor = true;
+            this.tabPage4.Click += new System.EventHandler(this.tabPage4_Click);
+            // 
+            // txtResultsLog
+            // 
+            this.txtResultsLog.Location = new System.Drawing.Point(6, 6);
+            this.txtResultsLog.Multiline = true;
+            this.txtResultsLog.Name = "txtResultsLog";
+            this.txtResultsLog.Size = new System.Drawing.Size(525, 608);
+            this.txtResultsLog.TabIndex = 3;
+            this.txtResultsLog.TextChanged += new System.EventHandler(this.textBox7_TextChanged_2);
+            // 
+            // avantiInsert
+            // 
+            this.avantiInsert.Location = new System.Drawing.Point(435, 617);
+            this.avantiInsert.Name = "avantiInsert";
+            this.avantiInsert.Size = new System.Drawing.Size(96, 23);
+            this.avantiInsert.TabIndex = 1;
+            this.avantiInsert.Text = "Insert";
+            this.avantiInsert.UseVisualStyleBackColor = true;
+            this.avantiInsert.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(6, 617);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(425, 20);
+            this.textBox6.TabIndex = 0;
+            this.textBox6.TextChanged += new System.EventHandler(this.textBox6_TextChanged);
+            // 
+            // tabPage3
+            // 
+            this.tabPage3.Location = new System.Drawing.Point(4, 22);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage3.Size = new System.Drawing.Size(539, 640);
+            this.tabPage3.TabIndex = 2;
+            this.tabPage3.Text = "365 Retail";
+            this.tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(539, 640);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "MicroTronics";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            this.tabPage2.Click += new System.EventHandler(this.tabPage2_Click);
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.textBox5);
+            this.tabPage1.Controls.Add(this.label3);
+            this.tabPage1.Controls.Add(this.label2);
+            this.tabPage1.Controls.Add(this.textBox4);
+            this.tabPage1.Controls.Add(this.button4);
+            this.tabPage1.Controls.Add(this.textBox3);
+            this.tabPage1.Controls.Add(this.button3);
+            this.tabPage1.Controls.Add(this.button2);
+            this.tabPage1.Controls.Add(this.Load32);
+            this.tabPage1.Controls.Add(this.button1);
+            this.tabPage1.Controls.Add(this.textBox2);
+            this.tabPage1.Controls.Add(this.textBox1);
+            this.tabPage1.Controls.Add(this.label1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(539, 640);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "32 Markets";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(328, 37);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(47, 20);
+            this.textBox5.TabIndex = 12;
+            this.textBox5.Text = "5984";
+            this.textBox5.TextChanged += new System.EventHandler(this.textBox5_TextChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(296, 40);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(26, 13);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Port";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(154, 40);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(51, 13);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Couch IP";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(211, 37);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(79, 20);
+            this.textBox4.TabIndex = 9;
+            this.textBox4.Text = "127.0.0.1";
+            this.textBox4.TextChanged += new System.EventHandler(this.textBox4_TextChanged);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(347, 6);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(28, 22);
+            this.button4.TabIndex = 8;
+            this.button4.Text = "...";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click_1);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(104, 7);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(237, 20);
+            this.textBox3.TabIndex = 7;
+            this.textBox3.Text = "C:\\Apache Software Foundation\\CouchDB\\etc\\couchdb\\local.ini";
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(381, 7);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(150, 23);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "Gain Access";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(381, 35);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(150, 23);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Try CouchDB";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Load32
+            // 
+            this.Load32.Location = new System.Drawing.Point(8, 6);
+            this.Load32.Name = "Load32";
+            this.Load32.Size = new System.Drawing.Size(75, 23);
+            this.Load32.TabIndex = 4;
+            this.Load32.Text = "Load Json";
+            this.Load32.UseVisualStyleBackColor = true;
+            this.Load32.Click += new System.EventHandler(this.Load32_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(450, 617);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(86, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Insert";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(3, 620);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(441, 20);
+            this.textBox2.TabIndex = 3;
+            this.textBox2.Text = "Data Source=.;Initial Catalog=deORO_Local;Integrated Security=True";
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.textBox1.Location = new System.Drawing.Point(6, 71);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(3, 20, 3, 3);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox1.Size = new System.Drawing.Size(525, 530);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Text = "\r\n";
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(3, 604);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(91, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "ConnectionString:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Controls.Add(this.tabPage3);
+            this.tabControl1.Controls.Add(this.tabPage4);
+            this.tabControl1.Location = new System.Drawing.Point(0, 0);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(547, 666);
+            this.tabControl1.TabIndex = 5;
+            // 
+            // openFileDialog32Couch
+            // 
+            this.openFileDialog32Couch.FileName = "openFileDialog32Couch";
+            this.openFileDialog32Couch.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog32Couch_FileOk);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(547, 665);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.tabPage4.ResumeLayout(false);
+            this.tabPage4.PerformLayout();
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.tabControl1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.OpenFileDialog openFileDialog32;
+        private System.Windows.Forms.TabPage tabPage4;
+        private System.Windows.Forms.TabPage tabPage3;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button Load32;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.OpenFileDialog openFileDialog32Couch;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button avantiInsert;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox txtResultsLog;
+    }
+}
+
